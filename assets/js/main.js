c18d540fa2140d5ffc6e5623bf4039ab4bf1c593
thomas <46255836+Nyumat@users.noreply.github.com>
2020-07-04 03:59:31 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 /*
-	Forty by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	** S/o to @ajlkn for this well commented JS Code. Saved me countless hours. **
 */
 
 (function($) {
