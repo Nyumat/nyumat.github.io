7fec2f5ab24d5f2f346e321f480ef48b5c4b270e
thomas <46255836+Nyumat@users.noreply.github.com>
2020-08-15 00:18:46 -0400

    Create launchScrn.js

---
+++ assets/js/launchScrn.js
@@ -0,0 +1,21 @@
+/*
+Will contain launch screen javaScript code.
+The main reason for this is because my website in its current state
+Doesn't really have  a great landing/startup animation that is done yet.
+*/
+
+var onExit;
+var user_cookie = document.cookie
+$(selector).delay(2000).fadeIn("Animation");
+
+      if visit == first_visit {
+      
+        onExit = $selector.timeDelay().click(function);
+        user.cookie = "username=" + (name); expires=(onExit);
+        
+        $('.enter_link').click(function() {
+               $(this).parent().fadeOut(500);
+               
+         };
+      };
+   };         
